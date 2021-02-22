017531b94972a7ff8b9fb2eaf3aa38bb428cb517
p-jaeeun <j_brick@naver.com>
2021-02-22 20:11:26 +0900

    login 기능구현

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { User } = require("./models/User");
 
@@ -12,7 +13,7 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json 타입을 분석해줌
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 // mongoDB 연결
 const mongoose = require("mongoose");
 
@@ -24,7 +25,7 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => console.log("MongoDB is Connected..."))
-  .catch((error) => console.log(error));
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!!");
@@ -34,11 +35,43 @@ app.post("/register", (req, res) => {
   // 회원가입 시 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 보내준다.
 
   const user = new User(req.body);
-
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "입력한 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일이 데이터베이스에 있다면, 비밀번호가 일치하는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+      // 비밀번호까지 일치한다면, 토큰을 생성
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+
+        // 토큰을 저장한다.  어디에? 여기선 cookie에 저장해보겠음
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -8,10 +11,11 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true, // ex) 유저가 john ahn@naver.com라고 입력하면  trim이 공백을 없애줌
+    unique: 1,
   },
   password: {
     type: String,
-    maxlength: 50,
+    minlength: 5,
   },
   lastname: {
     type: String,
@@ -26,10 +30,59 @@ const userSchema = mongoose.Schema({
     type: String,
   },
   tokenExp: {
-    type: String,
+    type: Number,
   },
 });
 
+// 몽구스 메서드 유저정보를 저장하기 전에 함수 수행
+userSchema.pre("save", function (next) {
+  var user = this; //위에있는 user를 가르킴.
+
+  // 유저가 비밀번호를 변경했을 때
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash; // hash : 암호화된 비밀번호
+        next();
+      });
+    });
+  } else {
+    // 유저가 비밀번호가 아닌 다른 정보를 변경했을 경우,
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  // 입력한 비밀번호와, 암호화된 비밀번호 비교하기
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) {
+      return callback(err);
+    } else {
+      callback(null, isMatch); // err없고, isMatch=true
+    }
+  });
+};
+
+userSchema.methods.generateToken = function (callback) {
+  var user = this; //es5문법
+  // console.log("user._id", user._id);
+  // jsonwebtoken 이용해서 token 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  // user._id + "secretToken" = token
+  // -> 'secretToken' -> user._id
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) {
+      return callback(err);
+    } else callback(null, user);
+  });
+};
 // model이 schema를 감싼다
 const User = mongoose.model("User", userSchema);
 
